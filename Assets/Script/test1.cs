432b57cd8aaf0bfebc432b147ffba1deb39fa86a
Yanai1005 <tatsuya871_1005@icloud.com>
2024-06-15 00:58:27 +0900

    設定

--- Assets/Script/test1.cs
+++ Assets/Script/test1.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public class test1 : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
-        Debug.Log("Helo World!");
+        var prefab = await Addressables.LoadAssetAsync<GameObject>("universal / Runtime / Materials / Lit.mat.universal / Runtime / Materials / Lit.mat").Task;
+        Instantiate(prefab, new Vector3(0.0f, 0.0f, -8.0f), Quaternion.Euler(0f, 180f, 0f));
     }
 
     // Update is called once per frame
